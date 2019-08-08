9893a0e914caca839033676141eb5030722f22c9
Brian Plantico <bplantico@gmail.com>
2019-08-08 15:22:57 -0600

    Fixes games seed file and adds routes for games index and show routes

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -5,14 +5,30 @@ var Game = require('../../../models').Game;
 /* GET all games */
 router.get("/", function(req, res, next) {
   Game.findAll()
-  .then(games => {
+    .then(games => {
       res.setHeader("Content-Type", "application/json");
       res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+router.get("/:id", function(req, res, next) {
+  Game.findAll({
+    where: {
+      id: req.params.id
+    }
   })
-  .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({error})
-  });
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
 });
 
 module.exports = router;
